bd195ae25a91b6594d148f797fb9adb9b6951d83
Andrii <94639278+nagornuuu@users.noreply.github.com>
2023-06-10 18:34:41 +0200

    commit

---
+++ czwiczenia 11/cwiczenia11_mp-s24918/Czwiczenia11/Czwiczenia11/Data/Student.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Czwiczenia11.Data
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string Avatar { get; set; } = null!;
+        public string FirstName { get; set; } = null;
+        public string LastName { get; set; } = null;
+        
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+        public string Studies { get; set; } = null;
+    }
+}
